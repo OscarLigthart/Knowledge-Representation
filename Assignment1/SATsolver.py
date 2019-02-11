9ce6b1ef8a9a6abaf9ea1ea68bfa6ba6820c622a
Oscar Ligthart <oscarligthart@gmail.com>
2019-02-11 10:51:14 +0100

    Changed structure

---
+++ Assignment1/SATsolver.py
@@ -0,0 +1,18 @@
+
+def SATsolver(sud_input, rules_input):
+    '''
+    This function takes as input the DIMACS format of a sudoku and the DIMACS format of the sudoku rules to correctly
+    Solve a sudoku
+    '''
+
+    # read the sudoku input
+
+
+    # read the rules
+
+
+    # create clausules of a solved sudoku
+
+    return
+
+

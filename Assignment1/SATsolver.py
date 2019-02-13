cf3c9ddfc6b45905828a63094f812a777a7c7e94
Oscar Ligthart <oscarligthart@gmail.com>
2019-02-13 11:48:52 +0100

    merge

--- Assignment1/SATsolver.py
+++ Assignment1/SATsolver.py
@@ -1,5 +1,69 @@
 import itertools as it
 import numpy as np
+import random
+
+class Node:
+    """
+    Class Node
+    """
+    def __init__(self, dictionary):
+        self.left = None
+        self.data = dictionary
+        self.right = None
+
+class Tree:
+    """
+    Class tree will provide a tree as well as utility functions.
+    """
+
+    def createNode(self, data):
+        """
+        Utility function to create a node.
+        """
+        return Node(data)
+
+    def insert(self, node , data):
+        """
+        Insert function will insert a node into tree.
+        Duplicate keys are not allowed.
+        """
+        #if tree is empty , return a root node
+        if node is None:
+            return self.createNode(data)
+        # if data is smaller than parent , insert it into left side
+        if data < node.data:
+            node.left = self.insert(node.left, data)
+        elif data > node.data:
+            node.right = self.insert(node.right, data)
+
+        return node
+
+def unit_clause_simplification(problem, data, it):
+    """
+    This function performs a unit clause simplification
+
+    :param problem: total list of left clauses
+    :param data: all variables and the value they represent
+    :param it: the index of the current clause
+
+    :return:
+    """
+
+    var = problem[it][0]
+
+    try:
+        data['unk'].remove(abs(var))
+    except:
+        pass
+
+    # if the variable is positive, it has to be true in order for the clause to be true
+    if var > 0:
+        data['true'].append(abs(var))
+    # and vice versa
+    else:
+        data['false'].append(abs(var))
+
+    return data
 
 
 def SATsolver(sud_input, rules_input):
@@ -98,15 +162,23 @@ def SATsolver(sud_input, rules_input):
     count = 0
     while len(problem) > 0 and count < 1:
         count += 1
-        print(len(problem))
-        # step 1, simplify using initial sudoku
+
+        #########################################
+        # step 1, simplify using initial sudoku #
+        #########################################
 
         for i, clause in enumerate(problem):
+
             for j, literal in enumerate(clause):
+
+                # TODO check if right elements are deleted
                 if abs(literal) in data['true']:
+
                     # check if it is a negative, remove the literal if it is a negative
+                    # TODO create function for this
                     if literal < 0:
                         del problem[i][j]
+
                     # remove the clause if literal within is true
                     else:
                         del problem[i]
@@ -115,17 +187,54 @@ def SATsolver(sud_input, rules_input):
                     # check if it is a positive, remove the literal if it is a positive
                     if literal > 0:
                         del problem[i][j]
+
                     # remove the clause if literal within is false
                     else:
                         del problem[i]
 
-        # step 2, simplify using rules
+        ################################
+        # step 2, simplify using rules #
+        ################################
 
-        print(len(problem))
+        # unit clause simplification
+        for i, clause in enumerate(problem):
+            if len(clause) == 1:
+                data = unit_clause_simplification(problem, data, i)
 
-        # step 3, split if necessary
+        data['false'] = list(set(data['false']))
+        data['true'] = list(set(data['true']))
 
-        # step 4, rinse and repeat
+
+        # sanity check
+        # TODO, check if lists hold unique elements accross, element can't be true and false
+        #print(data['false'])
+
+        ##############################
+        # step 3, split if necessary #
+        ##############################
+
+        # check if necessary
+        if len(problem) > 1:
+
+            # save current state (as node)
+
+
+            # apply split
+            # pick random value from unknowns and set it to either true or false
+            literal = random.choice(data['unk'])
+            data['unk'].remove(literal)
+
+            if random.random >= 0.5:
+                data['true'].append(literal)
+            else:
+                data['false'].append(literal)
+
+
+
+
+        ############################
+        # step 4, rinse and repeat #
+        ############################
 
     return
 

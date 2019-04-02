4f85a5f7fe56d219312d73c4bc559d5d590195ce
Oscar Ligthart <oscarligthart@gmail.com>
2019-04-02 14:47:33 +0200

    changed state representation

--- Assignment2/waterflow.py
+++ Assignment2/waterflow.py
@@ -15,7 +15,7 @@ quantities = {
 
 derivatives = ['-', '0', '+']
 
-all_permutations = [quantities['I'], derivatives, quantities['V'], derivatives, quantities['O'], derivatives]
+all_permutations = [quantities['I'], quantities['V'], quantities['O'], derivatives, derivatives, derivatives]
 
 pre_states = list(itertools.product(*all_permutations))
 print(pre_states)
@@ -24,17 +24,12 @@ print(len(pre_states))
 states = []
 # order the states --> two lists where one is magnitude and the other is derivative
 for state in pre_states:
-    magnitudes = []
-    d = []
-    for i, value in enumerate(state):
-        if i % 2 == 0:
-            magnitudes.append(value)
-        else:
-            d.append(value)
-
-    states.append([magnitudes, d])
-
-print(states)
+    new_state = {}
+    for i, key in enumerate(quantities.keys()):
+        new_state[key] = {}
+        new_state[key]['magnitude'] = state[i]
+        new_state[key]['derivative'] = state[3+i]
+    states.append(new_state)
 
 # introduce dependencies
 # I --(I+)--> V
@@ -42,6 +37,16 @@ print(states)
 # V --(P+)--> O
 
 # get beginning state, find states that fit the dependencies?
+# what is the beginning state?
+# start with an empty tub and open the tap
+
+# for every state get the 
+
+# find new way to save data
+# how to save data?
+
+
+
 
 
 
